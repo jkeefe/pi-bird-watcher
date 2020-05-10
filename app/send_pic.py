4aa0943d8353fe62f60162e24b5a5126a14031a1
John Keefe <john@johnkeefe.net>
2020-05-09 20:19:03 -0400

    made it a command-line tool

--- app/send_pic.py
+++ app/send_pic.py
@@ -6,13 +6,21 @@
 
 import boto3
 import requests
+import sys
 
 
-# In[8]:
+# In[ ]:
 
 
-image_name = 'bird-article-l.png'
-image_path = '../testdata/' + image_name
+image_path = sys.argv[1]
+if !image_path:
+    print("Need a file name to check!")
+    sys.exit(1)
+
+
+# In[10]:
+
+
 analyzer_url = 'https://pi-bird-watcher.onrender.com/checkurl'
 bucket = 'pi-bird-images'
 bucket_url = 'https://pi-bird-images.s3.amazonaws.com/'
@@ -23,7 +31,7 @@ bucket_url = 'https://pi-bird-images.s3.amazonaws.com/'
 
 s3 = boto3.client('s3')
 s3.upload_file(
-    image_path, bucket, image_name,
+    image_path, bucket, image_path,
     ExtraArgs={'ACL': 'public-read'}
 )
 
@@ -31,6 +39,6 @@ s3.upload_file(
 # In[9]:
 
 
-url = bucket_url + image_name
+url = bucket_url + image_path
 response = requests.post(analyzer_url, json={ 'url': url })
 
